5bb7c37eda53c5431c9e61ee3e8565dd9dcd7ca1
Michael Chan <mchan10@stuy.edu>
2019-04-02 10:37:01 -0400

    added methods for linekdlist

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,27 @@
 public class MyLinkedList<E>{
   private Node start;
   private Node end;
+  public MyLinkedList(){
+  }
+  public void clear(){
+    start = null;
+    end = null;
+  }
+  public boolean add(Node n){
+    if (start == null){
+      start = n;
+      return true;
+    }
+    end.setNext(n);
+    end = n;
+    return true;
+  }
+  public void extend(MyLinkedList<E> other){
+    end.setNext(other.start);
+    end = other.end;
+  }
+  public E removeFront(){
+    start = start.getNext();
+    return start.getData();
+  }
 }
